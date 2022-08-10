e1aaed786b8e1639bd5b55902b48219e84cc268a
sachin <91408607+sachinsmr@users.noreply.github.com>
2022-08-10 17:45:45 +0530

    updated

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-console.log('Hello There !!!');
+const express = require("express");
+const app = express();
 
-console.log('Second line');
+app.get("/", (req, res) => {
+  res.send("<h1>welcome to node app</h1>");
+});
+
+app.listen(8000, {} => {
+console.log('Hello There !!!');
+});
